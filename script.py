fe795ae7e0f9de18b663f0c8e40a1ccf6998110c
Tanisha Belkar <tanisha.belkar@peak.ai>
2024-03-28 15:53:10 +0530

    input params

--- script.py
+++ script.py
@@ -1,5 +1,12 @@
-import sys
+import argparse
 
-args = sys.argv
+parser = argparse.ArgumentParser()
+parser.add_argument("--name", help="name", required=True)
+parser.add_argument("--age", help="age", required=True)
+parser.add_argument("--city", help="city")
 
-print(args)
+choice = input("enter choice- ")
+
+args = parser.parse_args()
+name, city = args
+print(name + " " + city + " " + choice)
