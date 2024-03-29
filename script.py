68e5bd36db40210e8ef8f260724a64b784499541
Tanisha Belkar <tanisha.belkar@peak.ai>
2024-03-29 12:01:41 +0530

    manual interaction

--- script.py
+++ script.py
@@ -5,8 +5,10 @@ parser.add_argument("--name", help="name", required=True)
 parser.add_argument("--age", help="age", required=True)
 parser.add_argument("--city", help="city")
 
-choice = input("enter choice- ")
-
 args = parser.parse_args()
+
+if args.age < 20:
+    choice = input("enter choice- ")
+
 name, city = args
 print(name + " " + city + " " + choice)
