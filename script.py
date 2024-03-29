9e9d8817658a22b30af8eaa703e3c650ddceb251
Tanisha Belkar <tanisha.belkar@peak.ai>
2024-03-29 12:27:45 +0530

    manual interaction bug fix

--- script.py
+++ script.py
@@ -6,7 +6,7 @@ parser.add_argument("--age", help="age", required=True)
 parser.add_argument("--city", help="city")
 
 args = parser.parse_args()
-
+choice = 'n'
 if args.age != '15':
     choice = input("enter choice- ")
 
