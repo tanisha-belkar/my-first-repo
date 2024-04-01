934efd537365561a3016ef61caa05643ed9d51cf
Tanisha Belkar <tanisha.belkar@peak.ai>
2024-04-01 19:01:53 +0530

    123

--- script.py
+++ script.py
@@ -1,9 +1,15 @@
 import argparse
 
 parser = argparse.ArgumentParser()
-parser.add_argument("--name", help="name", required=True)
-parser.add_argument("--age", help="age", required=True)
-parser.add_argument("--city", help="city")
+parser.add_argument("--api-key", help="name", required=True)
+parser.add_argument("--stage", help="age", required=True)
+parser.add_argument("--dry-run", help="city")
+
+parser.add_argument("--blocks", help="city")
+parser.add_argument("--exclude-blocks", help="city")
+
+parser.add_argument("--deployments", help="city")
+parser.add_argument("--exclude-deployments", help="city")
 
 args = parser.parse_args()
 choice = 'n'
